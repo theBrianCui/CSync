b728808d8c7e0e806a69a89bad4acb21f5974026
Brian Cui <brian.cui@live.com>
2017-11-15 21:30:22 -0600

    WIP

--- time_test.c
+++ time_test.c
@@ -1,12 +1,25 @@
 #include <time.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+/* C(t) = H(t) + A(t)
+   = H(t) + m * H(t) + N */
+
+long get_software_clock()
+{
+    struct timespec hardware_monotonic;
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
     struct timespec monotonic;
-    if (clock_gettime(CLOCK_MONOTONIC_RAW, &monotonic) == 0) {
+    if (clock_gettime(CLOCK_MONOTONIC_RAW, &monotonic) == 0)
+    {
         printf("Time in seconds: %ld\n", monotonic.tv_sec);
         printf("Time in nanoseconds: %ld\n", monotonic.tv_nsec);
-    } else {
+    }
+    else
+    {
         printf("An unknown error occurred.\n");
     }
 }
