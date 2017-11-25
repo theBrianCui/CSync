f3a066357c9f9bd91f553f00015ddad1986d8f94
Brian Cui <brian.cui@live.com>
2017-11-25 15:45:14 -0600

    Cleaned up client code to be more consistent with server code

--- client.c
+++ client.c
@@ -42,10 +42,25 @@ int main(int argc, char *argv[])
     }
     last_print = last_rapport;
 
-    int sockfd, serverlen;
-    struct sockaddr_in serveraddr;
-    char buffer[MESSAGE_SIZE] = {0};
+    /* Create a socket for sending / receiving requests to the server */
+    int client_fd;
+    if ((client_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        printf("Socket creation failed. Exiting.\n");
+        exit(1);
+    }
 
+    /* Assign a receive timeout as a function of the POLL_PERIOD */
+    struct timeval timeout;
+    timeout.tv_sec = POLL_PERIOD / MILLION;
+    timeout.tv_usec = POLL_PERIOD % MILLION;
+    if (setsockopt(client_fd, SOL_SOCKET, SO_RCVTIMEO,
+                   &timeout, sizeof(timeout)) < 0) {
+        printf("Socket option assignment failed. Exiting.\n");
+    }
+
+    /* Build the server address struct with IP, port */
+    struct sockaddr_in server_addr;
+    int server_addr_len = sizeof(server_addr);
     if (argc < 3) {
         printf("Usage: client [ip address] [port]\n");
         exit(1);
@@ -53,35 +68,18 @@ int main(int argc, char *argv[])
 
     char *hostname = argv[1];
     int port = atoi(argv[2]);
+    server_addr.sin_family = AF_INET;
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        printf("Socket creation failed. Exiting.\n");
-        exit(1);
-    }
-
-    struct timeval timeout;
-    timeout.tv_sec = POLL_PERIOD / MILLION;
-    timeout.tv_usec = POLL_PERIOD % MILLION;
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,
-                   &timeout, sizeof(timeout)) < 0) {
-        printf("Socket option assignment failed. Exiting.\n");
-    }
-
-    serveraddr.sin_family = AF_INET;
-    inet_pton(AF_INET, hostname, &serveraddr.sin_addr);
-    serveraddr.sin_port = htons(port);
-    serverlen = sizeof(serveraddr);
+    /* Convert IP address from text to binary form */
+    inet_pton(AF_INET, hostname, &server_addr.sin_addr);
+    server_addr.sin_port = htons(port);
 
     uint32_t next_seq_num = 0;
     printf("Starting up...\n");
     while (1) {
-        memset(buffer, 0, MESSAGE_SIZE);
-
-        printf("Sending a message...\n");
-
         /* Construct message: [seq number] [query string] */
         char request_buffer[MESSAGE_SIZE] = {0};
+        char receive_buffer[MESSAGE_SIZE] = {0};
 
         /* Assign and increment sequence number */
         *(uint32_t *) request_buffer = htonl(next_seq_num++);
@@ -89,19 +87,20 @@ int main(int argc, char *argv[])
         /* Insert query string */
         strncpy(request_buffer + SEQ_NUM_SIZE, QUERY_STRING, PAYLOAD_SIZE);
 
-        sendto(sockfd, request_buffer, MESSAGE_SIZE, MSG_DONTWAIT,
-               (struct sockaddr *) &serveraddr, serverlen);
+        /* Send the request_buffer to the server */
+        sendto(client_fd, request_buffer, MESSAGE_SIZE, MSG_DONTWAIT,
+               (struct sockaddr *) &server_addr, server_addr_len);
 
-        /* Listen for incoming messages (will timeout) */
-        printf("Waiting for data...\n");
-        int recv_len = recvfrom(sockfd, buffer, MESSAGE_SIZE, 0,
-                                (struct sockaddr *) &serveraddr, &serverlen);
+        /* Listen for incoming messages (will eventually timeout) */
+        int recv_len = recvfrom(client_fd, receive_buffer, MESSAGE_SIZE, 0,
+                                (struct sockaddr *) &server_addr,
+                                &server_addr_len);
 
         if (recv_len == MESSAGE_SIZE) {
-            uint32_t received_seq_num = ntohl(*(uint32_t *) buffer);
+            uint32_t received_seq_num = ntohl(*(uint32_t *) receive_buffer);
             if (received_seq_num == next_seq_num) {
                 microts received_ts =
-                    ntohll(*(uint64_t *) (buffer + SEQ_NUM_SIZE));
+                    ntohll(*(uint64_t *) (receive_buffer + SEQ_NUM_SIZE));
 
                 printf("Received: [%d] [%ld]\n", received_seq_num, received_ts);
             }
