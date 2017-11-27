ee5640572511f744228ee8a1cb49fd2bab1eb0b1
briancui@VBOX-DEV0 <brian.cui@live.com>
2017-11-26 18:45:23 -0600

    Added missing POSIX #defines for timespec

--- sclock.c
+++ sclock.c
@@ -1,3 +1,8 @@
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
 #include "sclock.h"
 #include <time.h>
 #include <math.h>
