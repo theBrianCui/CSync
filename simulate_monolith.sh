3587cdbd9e8f15f2373e35935a58b1b2b9739dc9
Brian Cui <brian.cui@live.com>
2017-12-10 00:58:28 -0600

    Added monolith simulation code

--- simulate_monolith.sh
+++ simulate_monolith.sh
@@ -2,8 +2,10 @@
 drift="$1"
 rapport="$2"
 amort="$3"
+timeout="$4"
+dir="$5"
 
-mkdir -p "results/monolith/$drift"
-output="results/monolith/${drift}/${drift}_${rapport}_${amort}.txt"
+mkdir -p "results/${dir}/${drift}"
+output="results/${dir}/${drift}/${drift}_${rapport}_${amort}.txt"
 echo "Generating ${output}"
-./client 128.83.139.94 "80${drift}" "$drift" "-${drift}" -3.5 1800 "$rapport" 400000 "$amort" 50000 > "$output"
+./client 128.83.139.94 "80${drift}" "$drift" "-${drift}" -3.5 1800 "$rapport" "${timeout}" "$amort" 50000 > "$output"
